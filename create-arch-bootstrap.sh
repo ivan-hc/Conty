f591fc4fc043e711ab85db844d1a2e452fee5243
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-09-09 03:54:45 +0200

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -410,24 +410,22 @@ install -Dm 644 shrunk/linux.amd64/* \
 run_in_chroot rm -Rf /usr/include /usr/share/man
 run_in_chroot bash -c 'find "${bootstrap}"/usr/share/doc/* -not -iname "*virtualbox*" -a -not -name "." -delete'
 run_in_chroot bash -c 'find "${bootstrap}"/usr/share/locale/*/*/* -not -iname "*virtualbox*" -a -not -name "." -delete'
-rm -rf "${bootstrap}"/usr/lib/*.a
+#rm -rf "${bootstrap}"/usr/lib/*.a
+#rm -rf "${bootstrap}"/usr/lib/bellagio
+#rm -rf "${bootstrap}"/usr/lib/cmake/Qt*
 #rm -rf "${bootstrap}"/usr/lib/libgallium*
-rm -rf "${bootstrap}"/usr/lib/libgo.so*
-rm -rf "${bootstrap}"/usr/lib/libgphobos.so*
+#rm -rf "${bootstrap}"/usr/lib/libgo.so*
+#rm -rf "${bootstrap}"/usr/lib/libgphobos.so*
 #rm -rf "${bootstrap}"/usr/lib/libLLVM*
-rm -rf "${bootstrap}"/usr/lib/systemd
-rm -rf "${bootstrap}"/usr/share/info
-rm -rf "${bootstrap}"/usr/share/gir-1.0
-rm -rf "${bootstrap}"/var/lib/pacman/*
-
-rm -rf "${bootstrap}"/usr/lib/bellagio
-rm -rf "${bootstrap}"/usr/lib/cmake/Qt*
-rm -rf "${bootstrap}"/usr/share/fonts/*
-rm -rf "${bootstrap}"/usr/share/i18n
-
-strip --strip-debug "${bootstrap}"/usr/lib/*
-strip --strip-debug "${bootstrap}"/usr/lib32/*
-strip --strip-unneeded "${bootstrap}"/usr/bin/*
+#rm -rf "${bootstrap}"/usr/lib/systemd
+#rm -rf "${bootstrap}"/usr/share/fonts/*
+#rm -rf "${bootstrap}"/usr/share/gir-1.0
+#rm -rf "${bootstrap}"/usr/share/i18n
+#rm -rf "${bootstrap}"/usr/share/info
+#rm -rf "${bootstrap}"/var/lib/pacman/*
+#strip --strip-debug "${bootstrap}"/usr/lib/*
+#strip --strip-debug "${bootstrap}"/usr/lib32/*
+#strip --strip-unneeded "${bootstrap}"/usr/bin/*
 
 # Check if the command we are interested in has been installed
 if ! run_in_chroot which virtualbox; then echo "Command not found, exiting." && exit 1; fi
