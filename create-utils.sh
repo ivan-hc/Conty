d11307c23df2d4ec2cd3a534b2adf257b4f747cb
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-13 14:11:34 -0400

    Switch to some static tools (#1)
    * Switch to some static bins
    
    * Update create-utils.sh
    
    * Update create-utils.sh
    
    * Update create-utils.sh
    
    * Update utils.yml
    
    * Update utils.yml
    
    * Update create-utils.sh

--- create-utils.sh
+++ create-utils.sh
@@ -13,13 +13,9 @@
 script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
 # Set to true to compile dwarfs instead of squashfuse
-build_dwarfs="${build_dwarfs:-false}"
-
+build_dwarfs=true
 squashfuse_version="0.5.2"
 bwrap_version="0.10.0"
-lz4_version="1.10.0"
-zstd_version="1.5.6"
-squashfs_tools_version="4.6.1"
 unionfs_fuse_version="3.3"
 busybox_version="1.36.1"
 bash_version="5.2.32"
@@ -34,53 +30,22 @@ export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
 mkdir -p "${script_dir}"/build-utils
 cd "${script_dir}"/build-utils || exit 1
 
-curl -#Lo lz4.tar.gz https://github.com/lz4/lz4/archive/refs/tags/v${lz4_version}.tar.gz
-curl -#Lo zstd.tar.gz https://github.com/facebook/zstd/archive/refs/tags/v${zstd_version}.tar.gz
 curl -#Lo bwrap.tar.gz https://github.com/containers/bubblewrap/archive/refs/tags/v${bwrap_version}.tar.gz
-curl -#Lo unionfs-fuse.tar.gz https://github.com/rpodgorny/unionfs-fuse/archive/refs/tags/v${unionfs_fuse_version}.tar.gz
 curl -#Lo busybox.tar.bz2 https://busybox.net/downloads/busybox-${busybox_version}.tar.bz2
 curl -#Lo bash.tar.gz https://ftp.gnu.org/gnu/bash/bash-${bash_version}.tar.gz
 cp "${script_dir}"/init.c init.c
 
-tar xf lz4.tar.gz
-tar xf zstd.tar.gz
 tar xf bwrap.tar.gz
-tar xf unionfs-fuse.tar.gz
 tar xf busybox.tar.bz2
 tar xf bash.tar.gz
-
-if [ "${build_dwarfs}" != "true" ]; then
-	curl -#Lo squashfuse.tar.gz https://github.com/vasi/squashfuse/archive/refs/tags/${squashfuse_version}.tar.gz
-	curl -#Lo sqfstools.tar.gz https://github.com/plougher/squashfs-tools/archive/refs/tags/${squashfs_tools_version}.tar.gz
-
-	tar xf squashfuse.tar.gz
-	tar xf sqfstools.tar.gz
-fi
 
 cd bubblewrap-"${bwrap_version}" || exit 1
 # Apply bwrap patch
 wget -q https://raw.githubusercontent.com/Samueru-sama/Conty/refs/heads/master/caps.patch
-patch < caps.patch || exit 1             
+patch < caps.patch || exit 1
 ./autogen.sh
 ./configure --disable-selinux --disable-man
 make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
-
-cd ../unionfs-fuse-"${unionfs_fuse_version}" || exit 1
-mkdir build-fuse3
-cd build-fuse3
-cmake ../ -DCMAKE_BUILD_TYPE=Release
-make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
-mv "${script_dir}"/build-utils/bin/usr/local/bin/unionfs "${script_dir}"/build-utils/bin/usr/local/bin/unionfs3
-mkdir ../build-fuse2
-cd ../build-fuse2
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DWITH_LIBFUSE3=FALSE
-make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
-
-cd ../../lz4-"${lz4_version}" || exit 1
-make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
-
-cd ../zstd-"${zstd_version}" || exit 1
-ZSTD_LEGACY_SUPPORT=0 HAVE_ZLIB=0 HAVE_LZMA=0 HAVE_LZ4=0 BACKTRACE=0 make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
 
 cd ../busybox-${busybox_version} || exit 1
 make defconfig
@@ -95,59 +60,21 @@ autoconf -f
 CFLAGS="${CFLAGS} -Wno-error=implicit-function-declaration -static" CC=musl-gcc ./configure --without-bash-malloc
 CFLAGS="${CFLAGS} -Wno-error=implicit-function-declaration -static" CC=musl-gcc make -j"$(nproc)"
 
-if [ "${build_dwarfs}" != "true" ]; then
-	cd ../squashfuse-"${squashfuse_version}" || exit 1
-	./autogen.sh
-	./configure
-	make -j"$(nproc)" DESTDIR="${script_dir}"/build-utils/bin install
-
-	cd ../squashfs-tools-"${squashfs_tools_version}"/squashfs-tools || exit 1
-	CC=gcc CXX=g++ make -j"$(nproc)" GZIP_SUPPORT=1 XZ_SUPPORT=1 LZO_SUPPORT=1 LZMA_XZ_SUPPORT=1 \
-			LZ4_SUPPORT=1 ZSTD_SUPPORT=1 XATTR_SUPPORT=1
-	CC=gcc CXX=g++ make INSTALL_DIR="${script_dir}"/build-utils/bin/usr/local/bin install
-fi
-
 cd "${script_dir}"/build-utils || exit 1
 mkdir utils
 mv bin/usr/local/bin/bwrap utils
-mv bin/usr/local/bin/squashfuse utils
-mv bin/usr/local/bin/squashfuse_ll utils
-mv bin/usr/local/bin/mksquashfs utils
-mv bin/usr/local/bin/unsquashfs utils
-mv bin/usr/local/bin/unionfs3 utils
-mv bin/usr/local/bin/unionfs utils
-mv bin/usr/local/lib/liblz4.so."${lz4_version}" utils/liblz4.so.1
-mv bin/usr/local/lib/libzstd.so."${zstd_version}" utils/libzstd.so.1
-mv bin/usr/local/lib/libfuseprivate.so.0.0.0 utils/libfuseprivate.so.0
-mv bin/usr/local/lib/libsquashfuse.so.0.0.0 utils/libsquashfuse.so.0
+
+wget "https://bin.ajam.dev/x86_64_Linux/Baseutils/unionfs-fuse/unionfs" -O ./utils/unionfs
+wget "https://bin.ajam.dev/x86_64_Linux/Baseutils/unionfs-fuse3/unionfs" -O ./utils/unionfs3
+wget "https://bin.ajam.dev/x86_64_Linux/dwarfs-tools" -O ./utils/dwarfs-tools
+ln -s dwarfs-tools ./utils/dwarfs
+ln -s dwarfs-tools ./utils/mkdwarfs
+ln -s dwarfs-tools ./utils/dwarfsextract
+chmod +x ./utils/*
+
 mv "${script_dir}"/build-utils/busybox-${busybox_version}/busybox utils
 mv "${script_dir}"/build-utils/bash-${bash_version}/bash utils
 mv "${script_dir}"/build-utils/init utils
-
-if ! ldd utils/squashfuse | grep -q libfuse.so.2; then
-	mv utils/squashfuse utils/squashfuse3
-	mv utils/squashfuse_ll utils/squashfuse3_ll
-fi
-
-if [ "${build_dwarfs}" = "true" ]; then
-	git clone https://github.com/mhx/dwarfs.git --recursive
-
-	cd dwarfs || exit 1
-	mkdir build
-	cd build || exit 1
-	cmake .. -DCMAKE_BUILD_TYPE=Release \
-			-DPREFER_SYSTEM_ZSTD=ON -DPREFER_SYSTEM_XXHASH=ON \
-			-DPREFER_SYSTEM_GTEST=ON -DPREFER_SYSTEM_LIBFMT=ON
-
-	make -j"$(nproc)"
-	make DESTDIR="${script_dir}"/build-utils/bin install
-
-	cd "${script_dir}"/build-utils || exit 1
-	mv bin/usr/local/sbin/dwarfs2 utils/dwarfs
-	mv bin/usr/local/sbin/dwarfs utils/dwarfs3
-	mv bin/usr/local/bin/mkdwarfs utils
-	mv bin/usr/local/bin/dwarfsextract utils
-fi
 
 mapfile -t libs_list < <(ldd utils/* | awk '/=> \// {print $3}')
 
@@ -194,16 +121,10 @@ lz4 ${lz4_version}
 zstd ${zstd_version}
 EOF
 
-if [ "${build_dwarfs}" = "true" ]; then
-	echo "dwarfs $(git -C dwarfs rev-parse --short HEAD)-git" >> utils/info
-	utils="utils_dwarfs.tar.gz"
-else
-	echo "squashfuse ${squashfuse_version}" >> utils/info
-	echo "squashfs-tools ${squashfs_tools_version}" >> utils/info
-	utils="utils.tar.gz"
-fi
+echo "https://bin.ajam.dev/x86_64_Linux/dwarfs-tools" >> utils/info
+utils="utils_dwarfs.tar.gz"
 
-tar -zcf "${utils}" utils
+tar -zvcf "${utils}" utils
 mv "${script_dir}"/"${utils}" "${script_dir}"/"${utils}".old
 mv "${utils}" "${script_dir}"
 cd "${script_dir}" || exit 1
